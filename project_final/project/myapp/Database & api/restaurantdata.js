cce82fd81775f548b0e819af7d78e2ef09abd947
smit bhatt <smit.bb@somaiya.edu.com>
2020-09-02 21:20:22 +0530

    api&database

---
+++ project_final/project/myapp/Database & api/restaurantdata.js
@@ -0,0 +1,540 @@
+db.restaurant.insertMany(
+    [{
+        "_id" : "1",
+        "name" : "Gulab",
+        "city_name" : "Delhi",
+        "city" : "1",
+        "area" : "11",
+        "locality" : "Pitampura, New Delhi",
+        "thumb" : "https://b.zmtcdn.com/data/pictures/chains/3/6303/640252389ddc3f264dd0e9f2741e73cd.jpg",
+        "cost" : 450,
+        "address" : "Shop 1, Plot D,  Pitampura, New Delhi Complex, Chincholi, Delhi-110006, Delhi",
+        "type" : [
+                {
+                        "mealtype" : "1",
+                        "name" : "breakfast"
+                },
+                {
+                        "mealtype" : "3",
+                        "name" : "dinner"
+                }
+        ],
+        "Cuisine" : [
+                {
+                        "cuisine" : "1",
+                        "name" : "North Indain"
+                },
+                {
+                        "cuisine" : "4",
+                        "name" : "Fast Food"
+                }
+        ]
+}
+{
+        "_id" : "2",
+        "name" : "Pandit Ji Paratha Hut",
+        "locality" : "Ashok Vihar Phase 2",
+        "city_name" : "Delhi",
+        "city" : "1",
+        "area" : "12",
+        "address" : "Shop 44, Plot C,  Ahok Vihar Phase 2,Chincholi, Delhi-110006, Delhi",
+        "thumb" : "https://b.zmtcdn.com/data/pictures/chains/3/6303/640252389ddc3f264dd0e9f2741e73cd.jpg",
+        "cost" : 230,
+        "contact_number" : "45352465",
+        "type" : [
+                {
+                        "mealtype" : "1",
+                        "name" : "breakfast"
+                },
+                {
+                        "mealtype" : "3",
+                        "name" : "dinner"
+                }
+        ],
+        "Cuisine" : [
+                {
+                        "cuisine" : "1",
+                        "name" : "North Indain"
+                },
+                {
+                        "cuisine" : "3",
+                        "name" : "Chinese"
+                }
+        ]
+}
+{
+        "_id" : "3",
+        "name" : "Food Adda",
+        "locality" : "Borivali West",
+        "city_name" : "Mumbai",
+        "city" : "2",
+        "area" : "21",
+        "address" : "Borivali West, Mumbai-210006, Mumbai",
+        "thumb" : "https://b.zmtcdn.com/data/pictures/7/18690357/0df7f4ca0c645a68a1657b1e69b015fa.jpg",
+        "cost" : 530,
+        "contact_number" : "467564",
+        "type" : [
+                {
+                        "mealtype" : "2",
+                        "name" : "lunch"
+                },
+                {
+                        "mealtype" : "3",
+                        "name" : "dinner"
+                }
+        ],
+        "Cuisine" : [
+                {
+                        "cuisine" : "3",
+                        "name" : "Chinese"
+                },
+                {
+                        "cuisine" : "4",
+                        "name" : "FastFood"
+                }
+        ]
+}
+{
+        "_id" : "4",
+        "name" : "Apna Punjab",
+        "locality" : "Magarpatta",
+        "city_name" : "Pune",
+        "city" : "3",
+        "area" : "31",
+        "address" : "Borivali West, Mumbai-210006, Mumbai",
+        "thumb" : "https://b.zmtcdn.com/data/res_imagery/6508401_RESTAURANT_21a925c42f2f93c9709e1945b9eae56f.jpg",
+        "cost" : 670,
+        "contact_number" : "6508401",
+        "type" : [
+                {
+                        "mealtype" : "4",
+                        "name" : "snacks"
+                },
+                {
+                        "mealtype" : "5",
+                        "name" : "drinks"
+                }
+        ],
+        "Cuisine" : [
+                {
+                        "cuisine" : "1",
+                        "name" : "North Indain"
+                },
+                {
+                        "cuisine" : "2",
+                        "name" : "South Indian"
+                }
+        ]
+}
+{
+        "_id" : "5",
+        "name" : "Empire Restaurant",
+        "locality" : "Rajajinagar",
+        "city_name" : "Bangalore",
+        "city" : "4",
+        "area" : "41",
+        "address" : "Rajajinagar, Bangalore-430006, Bangalore",
+        "thumb" : "https://b.zmtcdn.com/data/pictures/1/50471/bcf68da39dcfb0fe5bcfb742c337385e.jpg",
+        "cost" : 230,
+        "contact_number" : "8731537",
+        "type" : [
+                {
+                        "mealtype" : "1",
+                        "name" : "breakfast"
+                },
+                {
+                        "mealtype" : "5",
+                        "name" : "drinks"
+                }
+        ],
+        "Cuisine" : [
+                {
+                        "cuisine" : "2",
+                        "name" : "South Indian"
+                },
+                {
+                        "cuisine" : "4",
+                        "name" : "FastFood"
+                }
+        ]
+}
+{
+        "_id" : "6",
+        "name" : "Captain Sams",
+        "locality" : "Sector70, Chandigarh",
+        "city_name" : "Chandigarh",
+        "city" : "5",
+        "area" : "51",
+        "address" : "Sector70, Chandigarh-515436",
+        "thumb" : "https://b.zmtcdn.com/data/reviews_photos/c7a/634a2c0def8a8d044992aea9e7680c7a_1556257724.jpg",
+        "cost" : 630,
+        "contact_number" : "123334",
+        "type" : [
+                {
+                        "mealtype" : "5",
+                        "name" : "drinks"
+                },
+                {
+                        "mealtype" : "6",
+                        "name" : "nightlife"
+                }
+        ],
+        "Cuisine" : [
+                {
+                        "cuisine" : "1",
+                        "name" : "North Indain"
+                },
+                {
+                        "cuisine" : "4",
+                        "name" : "FastFood"
+                }
+        ]
+}
+{
+        "_id" : "7",
+        "name" : "AMA Cafe",
+        "city_name" : "Delhi",
+        "city" : "1",
+        "area" : "11",
+        "locality" : "Majnu ka Tila, New Delhi",
+        "thumb" : "https://b.zmtcdn.com/data/res_imagery/307374_RESTAURANT_6688d81a57b8da4bcf20d725de39a3d2.jpg",
+        "cost" : 450,
+        "address" : "House 6, New Colony, Majnu ka Tilla, New Delhi",
+        "type" : [
+                {
+                        "mealtype" : "2",
+                        "name" : "lunch"
+                },
+                {
+                        "mealtype" : "4",
+                        "name" : "lunch"
+                }
+        ],
+        "Cuisine" : [
+                {
+                        "cuisine" : "2",
+                        "name" : "South Indian"
+                },
+                {
+                        "cuisine" : "3",
+                        "name" : "Chinese"
+                }
+        ]
+}
+{
+        "_id" : "8",
+        "name" : "Punjabi Angithi",
+        "city_name" : "Delhi",
+        "city" : "1",
+        "area" : "11",
+        "locality" : "Paschim Vihar, New Delhi",
+        "thumb" : "https://b.zmtcdn.com/data/pictures/3/307113/54e0e60a17086184f1e5a44d7f580b54.png",
+        "cost" : 350,
+        "address" : "32-22, A 4, DDA Market, Paschim Vihar, New Delhi",
+        "type" : [
+                {
+                        "mealtype" : "2",
+                        "name" : "lunch"
+                },
+                {
+                        "mealtype" : "4",
+                        "name" : "lunch"
+                }
+        ],
+        "Cuisine" : [
+                {
+                        "cuisine" : "2",
+                        "name" : "South Indian"
+                },
+                {
+                        "cuisine" : "3",
+                        "name" : "Chinese"
+                }
+        ]
+}
+{
+        "_id" : "9",
+        "name" : "Rajinder Da Dhaba",
+        "city_name" : "Delhi",
+        "city" : "1",
+        "area" : "11",
+        "locality" : "Safdarjung, New Delhi",
+        "thumb" : "https://b.zmtcdn.com/data/pictures/9/7319/e1b7673ed0aa2993b55b177409d5596c.jpg",
+        "cost" : 380,
+        "address" : "AB 14, Safdarjung Enclave Market, Safdarjung, New Delhi",
+        "type" : [
+                {
+                        "mealtype" : "5",
+                        "name" : "drinks"
+                },
+                {
+                        "mealtype" : "6",
+                        "name" : "nightlife"
+                }
+        ],
+        "Cuisine" : [
+                {
+                        "cuisine" : "4",
+                        "name" : "Fast Food"
+                },
+                {
+                        "cuisine" : "5",
+                        "name" : "Street Food"
+                }
+        ]
+}
+{
+        "_id" : "10",
+        "name" : "Diggin",
+        "city_name" : "Delhi",
+        "city" : "1",
+        "area" : "11",
+        "locality" : "Chanakyapuri, New Delhi",
+        "thumb" : "https://b.zmtcdn.com/data/pictures/3/307113/54e0e60a17086184f1e5a44d7f580b54.png",
+        "cost" : 650,
+        "address" : "10, Santushti Shopping Complex, Race Course Road, Chanakyapuri, New Delhi",
+        "type" : [
+                {
+                        "mealtype" : "5",
+                        "name" : "drinks"
+                },
+                {
+                        "mealtype" : "6",
+                        "name" : "nightlife"
+                }
+        ],
+        "Cuisine" : [
+                {
+                        "cuisine" : "4",
+                        "name" : "Fast Food"
+                },
+                {
+                        "cuisine" : "5",
+                        "name" : "Street Food"
+                }
+        ]
+}
+{
+        "_id" : "11",
+        "name" : "Saravana Bhavan",
+        "city_name" : "Delhi",
+        "city" : "1",
+        "area" : "11",
+        "locality" : "Connaught Place, New Delhi",
+        "thumb" : "https://b.zmtcdn.com/data/res_imagery/899_CHAIN_8b679bb604a1a4f128971906e6027c05.jpg",
+        "cost" : 650,
+        "address" : "P-13, Connaught Circus, Connaught Place, New Delhi",
+        "type" : [
+                {
+                        "mealtype" : "5",
+                        "name" : "drinks"
+                },
+                {
+                        "mealtype" : "6",
+                        "name" : "nightlife"
+                }
+        ],
+        "Cuisine" : [
+                {
+                        "cuisine" : "1",
+                        "name" : "North Indian"
+                },
+                {
+                        "cuisine" : "2",
+                        "name" : "South Indian"
+                }
+        ]
+}
+{
+        "_id" : "12",
+        "name" : "Bukhara",
+        "city_name" : "Delhi",
+        "city" : "1",
+        "area" : "11",
+        "locality" : "Chanakyapuri, New Delhi",
+        "thumb" : "https://b.zmtcdn.com/data/res_imagery/2742_RESTAURANT_b42f02ef23f24c3bcc0b005606d34b63.jpg",
+        "cost" : 650,
+        "address" : "ITC Maurya, Chanakyapur, New Delhi",
+        "type" : [
+                {
+                        "mealtype" : "4",
+                        "name" : "drinks"
+                },
+                {
+                        "mealtype" : "6",
+                        "name" : "snacks"
+                }
+        ],
+        "Cuisine" : [
+                {
+                        "cuisine" : "1",
+                        "name" : "North Indian"
+                },
+                {
+                        "cuisine" : "3",
+                        "name" : "Chinese"
+                }
+        ]
+}
+{
+        "_id" : "13",
+        "name" : "Sarvodaya",
+        "locality" : "Andheri east",
+        "city_name" : "Mumbai",
+        "city" : "2",
+        "area" : "25",
+        "address" : "Nagardas road,Andheri east,Mumbai",
+        "thumb" : "https://content3.jdmagicbox.com/comp/mumbai/r3/022pxx22.xx22.160920172212.g8r3/catalogue/sarvodaya-pure-veg-restaurant-andheri-east-mumbai-home-delivery-restaurants-9yw0g0.jpg",
+        "cost" : 550,
+        "contact_number" : "2662465",
+        "type" : [
+                {
+                        "mealtype" : "1",
+                        "name" : "breakfast"
+                },
+                {
+                        "mealtype" : "3",
+                        "name" : "lunch"
+                }
+        ],
+        "Cuisine" : [
+                {
+                        "cuisine" : "1",
+                        "name" : "North Indain"
+                },
+                {
+                        "cuisine" : "4",
+                        "name" : "Fast Food"
+                }
+        ]
+}
+{
+        "_id" : "14",
+        "name" : "Shabari",
+        "locality" : "Mira Road",
+        "city_name" : "Mumbai",
+        "city" : "2",
+        "area" : "22",
+        "address" : "near station Mira Road,Mumbai",
+        "thumb" : "https://timesofindia.indiatimes.com/thumb/msid-76206862,width-1200,height-900,resizemode-4/.jpg",
+        "cost" : 300,
+        "contact_number" : "2222465",
+        "type" : [
+                {
+                        "mealtype" : "6",
+                        "name" : "nightlife"
+                },
+                {
+                        "mealtype" : "5",
+                        "name" : "drinks"
+                }
+        ],
+        "Cuisine" : [
+                {
+                        "cuisine" : "1",
+                        "name" : "North Indain"
+                },
+                {
+                        "cuisine" : "5",
+                        "name" : "Street Food"
+                }
+        ]
+}
+{
+        "_id" : "15",
+        "name" : "Shiro",
+        "locality" : "Koramangala 6th Block",
+        "city_name" : "Bangalore",
+        "city" : "4",
+        "area" : "42",
+        "address" : "Koramangala 6th Block,Banglore",
+        "thumb" : "https://d4t7t8y8xqo0t.cloudfront.net/resized/750X436/restaurant%2F223052%2Frestaurant220180222061132.jpg",
+        "cost" : 900,
+        "contact_number" : "4352465",
+        "type" : [
+                {
+                        "mealtype" : "3",
+                        "name" : "dinner"
+                },
+                {
+                        "mealtype" : "1",
+                        "name" : "breakfast"
+                }
+        ],
+        "Cuisine" : [
+                {
+                        "cuisine" : "2",
+                        "name" : "South Indain"
+                },
+                {
+                        "cuisine" : "3",
+                        "name" : "Chinese"
+                }
+        ]
+}
+{
+        "_id" : "16",
+        "name" : "Regenta Central",
+        "locality" : "Sector 28, Chandigarh",
+        "city_name" : "Chandigarh",
+        "city" : "5",
+        "area" : "52",
+        "address" : "road no 5,Sector 28, Chandigarh",
+        "thumb" : "https://cdn.sloshout.com/uploads/gallery/interior-of-barcode-ixc-chandigarh-industrial-area.JPG",
+        "cost" : 900,
+        "contact_number" : "4352465",
+        "type" : [
+                {
+                        "mealtype" : "6",
+                        "name" : "nightlife"
+                },
+                {
+                        "mealtype" : "5",
+                        "name" : "drinks"
+                }
+        ],
+        "Cuisine" : [
+                {
+                        "cuisine" : "4",
+                        "name" : "Fast Food"
+                },
+                {
+                        "cuisine" : "5",
+                        "name" : "Street Food"
+                }
+        ]
+}
+{
+        "_id" : "17",
+        "name" : "Arthur's Theme",
+        "locality" : "Koregaon Park",
+        "city_name" : "Pune",
+        "city" : "3",
+        "area" : "32",
+        "address" : "Koregaon Park, Chandigarh",
+        "thumb" : "https://media-cdn.tripadvisor.com/media/photo-s/06/98/f5/5f/arthurs-theme.jpg",
+        "cost" : 550,
+        "contact_number" : "6552465",
+        "type" : [
+                {
+                        "mealtype" : "2",
+                        "name" : "lunch"
+                },
+                {
+                        "mealtype" : "4",
+                        "name" : "snacks"
+                }
+        ],
+        "Cuisine" : [
+                {
+                        "cuisine" : "3",
+                        "name" : "Chinese"
+                },
+                {
+                        "cuisine" : "5",
+                        "name" : "Street Food"
+                }
+        ]
+}
+    ]
+)
